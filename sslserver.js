e8928d3547f9a6fd0c157c8c2898801968061ffc
Sam Roy <44692735+mrsamroy@users.noreply.github.com>
2019-09-24 15:30:12 -0500

    Created sslserver.js
    This is an SSL node server for testing.

---
+++ sslserver.js
@@ -0,0 +1,37 @@
+/* 
+* LetsEncryptED SSL server
+*/
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+
+const app = express();
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/---YOUR-DOMAIN-HERE---/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/---YOUR-DOMAIN-HERE---/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/---YOUR-DOMAIN-HERE---/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+app.use((req, res) => {
+	res.send('Hello there !');
+});
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
